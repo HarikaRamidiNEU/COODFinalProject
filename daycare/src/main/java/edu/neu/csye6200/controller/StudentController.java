c2c6393bdc5573d69d6f9009b7c56840b09b7f41
Harika Ramidi <ramidi.h@northeastern.edu>
2022-12-10 15:33:36 -0500

    final project COOD

---
+++ daycare/src/main/java/edu/neu/csye6200/controller/StudentController.java
@@ -0,0 +1,44 @@
+package edu.neu.csye6200.controller;
+
+import edu.neu.csye6200.model.Student;
+import edu.neu.csye6200.service.StudentService;
+import edu.neu.csye6200.util.TestDataUtilities;
+
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class StudentController {
+	
+	private String filePath = Paths.get("", "src/main/resources", "Students.csv").toAbsolutePath().toString();
+	
+    @Autowired
+    StudentService studentService;
+
+    public void addTestData(){
+
+        studentService.saveStudents(new TestDataUtilities().populateStudentData(filePath));
+    }
+
+    public void displayAllStudents(){
+        studentService.getAllStudents().forEach(System.out::println);
+    }
+
+    public List<Student> getStudents(){
+        return studentService.getAllStudents();
+    }
+    public void removeStudentById(Integer id){
+        studentService.deleteStudentById(id);
+    }
+    public Optional<Student> readStudentById(Integer id){
+        return studentService.getStudentById(id);
+    }
+
+    public void addStudent(String csv){
+        studentService.saveStudent(csv);
+    }
+}
---
+++ daycare/src/main/java/edu/neu/csye6200/controller/VaccinationController.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.neu.csye6200.controller;
+
+import edu.neu.csye6200.model.Immunization;
+import edu.neu.csye6200.service.VaccinationService;
+import edu.neu.csye6200.util.TestDataUtilities;
+
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class VaccinationController {
+	
+	private String filePath = Paths.get("", "src/main/resources", "Vaccines.csv").toAbsolutePath().toString();
+	
+    @Autowired
+    VaccinationService vaccinationService;
+
+    public void addTestData(){
+        vaccinationService.saveVaccinations(new TestDataUtilities().populateVaccineData(filePath));
+    }
+
+    public void displayAllVaccines(){
+        vaccinationService.getAllVaccinations().forEach(System.out::println);
+    }
+
+    public List<Immunization> getAllVaccines(){
+        return vaccinationService.getAllVaccinations();
+    }
+    public void removeVaccineById(Integer id){
+        vaccinationService.deleteVaccinationsById(id);
+    }
+    public Optional<Immunization> getVaccinesById(Integer id){
+        return vaccinationService.getVaccinationById(id);
+    }
+
+    public void addVaccine(String csv){
+        vaccinationService.saveVaccination(csv);
+    }
+}
